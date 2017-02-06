9f9b5c06d259642eab22f2fff18761feabff81e1
Ashley Reid <omgashuhleeyy@Ashleys-MacBook-Air.local>
2017-02-06 17:28:47 -0500

    jawsdb

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,1 @@
-CREATE DATABASE sequelizedburgers_db;
-USE sequelizedburgers_db;
-
-CREATE TABLE burgers(
-id int AUTO_INCREMENT,
-PRIMARY KEY (id),
-burger_name varchar(50) NULL,
-devoured BOOLEAN default false,
-date TIMESTAMP);
+CREATE DATABASE sequelizedBurgers_db;
